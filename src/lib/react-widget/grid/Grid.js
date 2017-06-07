08608100495cf37e18615c272d93f9a588d98df3
卢森 <lusen_cn@163.com>
2017-06-07 16:39:53 +0800

    格式变化

--- src/lib/react-widget/grid/Grid.js
+++ src/lib/react-widget/grid/Grid.js
@@ -4,7 +4,6 @@ import React, {Component, PropTypes} from 'react';
 import {isEmpty, isFunction, isNumber} from '../utils';
 import GridCell from './GridCell';
 
-
 /**
  * 列表组件
  */
@@ -45,24 +44,24 @@ export default class Grid extends Component {
     /**
 	 * 组件默认属性值
 	 */
-	static defaultProps = {
-		borderSpacing: 1,
+    static defaultProps = {
+        borderSpacing: 1,
         headerHeight: 30
-	}
+    }
 
     //--------------------------------------------------------------------------
 
-	constructor(props) {
-    	super(props);
+    constructor(props) {
+        super(props);
         this.fixBodyCt = 'fixBodyCt';
-	}
+    }
 
     render() {
         this.initStyle();
         return <div className='w-grid-ct' style={this.ctStyle}>
-			{this.headerView()}
-			{this.bodyView()}
-		</div>
+            {this.headerView()}
+            {this.bodyView()}
+        </div>
     }
 
     /**
@@ -72,20 +71,22 @@ export default class Grid extends Component {
         let {borderSpacing, headerHeight, isFixHeader, style, width} = this.props;
 
         // 列表容器样式
-        this.ctStyle = Object.assign({}, isFixHeader ? {position: 'relative'} : null, style, {
-    		width: width
-    	});
+        this.ctStyle = Object.assign({}, isFixHeader
+            ? {
+                position: 'relative'
+            }
+            : null, style, {width: width});
 
         // 表头悬浮显示时，表体容器样式
         isFixHeader && (this.fixedCtStyle = {
-		    top: (headerHeight || 30) + 'px'
-		});
+            top: (headerHeight || 30) + 'px'
+        });
 
         // 表头/表体table样式
         this.tableStyle = {
-			width: width,
-			borderSpacing: borderSpacing || 1
-		};
+            width: width,
+            borderSpacing: borderSpacing || 1
+        };
 
         this.calColWidth();
     }
@@ -93,50 +94,50 @@ export default class Grid extends Component {
     /**
 	 * 计算未设置width值的列的宽度（如果有列设置width为百分数，或width设置不规范，则不执行该计算）
 	 */
-	calColWidth() {
-		this.calColWidthVal = 'auto';
+    calColWidth() {
+        this.calColWidthVal = 'auto';
 
-		let {borderSpacing, columns, width} = this.props;
-		if (!width || !isNumber(width)) {
-			return;
-		}
-		if (columns == null) {
-			return;
-		}
+        let {borderSpacing, columns, width} = this.props;
+        if (!width || !isNumber(width)) {
+            return;
+        }
+        if (columns == null) {
+            return;
+        }
 
         // 是否计算每列宽度
-		let isNoCal = false;
+        let isNoCal = false;
         // 未设置宽度的列的数量
-		let noWidthCnt = 0;
+        let noWidthCnt = 0;
         // 用于平均计算的宽度值
-		let minusResult = width - (columns.length + 1) * (borderSpacing || 1);
-		columns.some(column => {
-			let itemWidth = column.width;
-			if (!itemWidth) {
-				noWidthCnt++;
-				return false;
-			}
-			if (itemWidth && (itemWidth *= 1) && !isNumber(itemWidth)) {
+        let minusResult = width - (columns.length + 1) * (borderSpacing || 1);
+        columns.some(column => {
+            let itemWidth = column.width;
+            if (!itemWidth) {
+                noWidthCnt++;
+                return false;
+            }
+            if (itemWidth && (itemWidth *= 1) && !isNumber(itemWidth)) {
                 // 有列设置了非数字的宽度，导致无法为每列计算宽度
-				isNoCal = true;
-				return true;
-			}
-			minusResult -= itemWidth;
-		});
+                isNoCal = true;
+                return true;
+            }
+            minusResult -= itemWidth;
+        });
 
-		if (!isNoCal && noWidthCnt) {
-			this.calColWidthVal = minusResult / noWidthCnt
-		}
-		(this.calColWidthVal <= 0) && (this.calColWidthVal = 'auto');
-	}
+        if (!isNoCal && noWidthCnt) {
+            this.calColWidthVal = minusResult / noWidthCnt
+        }
+        (this.calColWidthVal <= 0) && (this.calColWidthVal = 'auto');
+    }
 
     /**
      * 表头视图
      */
     headerView() {
         return <div className="w-grid-table" style={this.tableStyle}>
-			{this.rowView({}, -1)}
-		</div>
+            {this.rowView({}, -1)}
+        </div>
     }
 
     /**
@@ -151,48 +152,69 @@ export default class Grid extends Component {
             ref: this.fixBodyCt
         })
 
-		return <div {...fixedCtProps}>
+        return <div {...fixedCtProps}>
             <div className="w-grid-table" style={this.tableStyle}>
-    			{isEmpty(records) ? this.emptyView() :
-    				records.map((record, rowIndex) => this.rowView(record, rowIndex))}
-    		</div>
+                {isEmpty(records)
+                    ? this.emptyView()
+                    : records.map((record, rowIndex) => this.rowView(record, rowIndex))}
+            </div>
         </div>
     }
 
     /**
 	 * 表格行视图
 	 */
-	rowView(record, rowIndex) {
-		let {columns} = this.props;
-		let trProps = {
+    rowView(record, rowIndex) {
+        let {columns} = this.props;
+        let trProps = {
             className: 'w-grid-tr',
             key: `gridRow_${rowIndex}`
         }
 
-		return <ul {...trProps}>
-			{columns.map(column => {
+        return <ul {...trProps}>
+            {columns.map(column => {
                 let {
-                    name, width, headerCellView, bodyCellView,
-                    headerCellStyle, bodyCellStyle, headerTdStyle, bodyTdStyle
+                    name,
+                    width,
+                    headerCellView,
+                    bodyCellView,
+                    headerCellStyle,
+                    bodyCellStyle,
+                    headerTdStyle,
+                    bodyTdStyle
                 } = column;
                 width = width || this.calColWidthVal;
                 let isHeader = this.isHeaderRow(rowIndex);
 
                 let tdProps = {
-                    className: isHeader ? 'w-grid-th' : 'w-grid-td',
-        			key: `gridHeader_${name}`,
-        			style: Object.assign({}, isHeader ? headerTdStyle : bodyTdStyle,
-                        width ? {width} : null)
-        		}
-                let cellProps = {
-                    column, record, rowIndex, width,
-                    cellView: isHeader ? headerCellView : bodyCellView,
-                    style: isHeader ? headerCellStyle : bodyCellStyle
+                    className: isHeader
+                        ? 'w-grid-th'
+                        : 'w-grid-td',
+                    key: `gridHeader_${name}`,
+                    style: Object.assign({}, isHeader
+                        ? headerTdStyle
+                        : bodyTdStyle, width
+                        ? {
+                            width
+                        }
+                        : null)
                 }
-                return <li {...tdProps}><GridCell {...cellProps} /></li>
+                let cellProps = {
+                    column,
+                    record,
+                    rowIndex,
+                    width,
+                    cellView: isHeader
+                        ? headerCellView
+                        : bodyCellView,
+                    style: isHeader
+                        ? headerCellStyle
+                        : bodyCellStyle
+                }
+                return <li {...tdProps}><GridCell {...cellProps}/></li>
             })}
-		</ul>
-	}
+        </ul>
+    }
 
     /**
      * 是否是表头行
@@ -212,38 +234,44 @@ export default class Grid extends Component {
         let {body, header} = colInfo;
         !body && (body = {});
         !header && (header = {});
-        return rowIndex >= 0 ? body[name] : header[name];
+        return rowIndex >= 0
+            ? body[name]
+            : header[name];
     }
 
-	/**
+    /**
 	 * 空数据视图
 	 */
-	emptyView() {
-		let {emptyView} = this.props;
-		return isFunction(emptyView) ? emptyView() :
-			<div className="w-grid-empty"><i className="i-empty"></i></div>
-	}
+    emptyView() {
+        let {emptyView} = this.props;
+        return isFunction(emptyView)
+            ? emptyView()
+            : <div className="w-grid-empty">
+                <i className="i-empty"></i>
+            </div>
+    }
 
     //--------------------------------------------------------------------------
 
     /**
 	 * 滚动到指定位置
 	 */
-	scrollTo(pos) {
-		try {
-			if (!isEmpty(pos)) {
-				let el = this.refs[this.fixBodyCt];
-				el && (el.scrollTop = pos);
-			}
-		} catch (e) {
-		}
-	}
+    scrollTo(pos) {
+        try {
+            if (!isEmpty(pos)) {
+                let el = this.refs[this.fixBodyCt];
+                el && (el.scrollTop = pos);
+            }
+        } catch (e) {}
+    }
 
-	/**
+    /**
 	 * 获取滚动条位置
 	 */
-	getScrollTop() {
-		let el = this.refs[this.fixBodyCt];
-		return el ? (el.scrollTop || 0) : 0;
-	}
+    getScrollTop() {
+        let el = this.refs[this.fixBodyCt];
+        return el
+            ? (el.scrollTop || 0)
+            : 0;
+    }
 }
